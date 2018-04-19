9946cbdc814486f2f0410590d00422c57ac2f44a
god <zarrass@mail.ru>
2018-04-19 11:35:09 +0700

    Full Rename

--- GeoApp/Startup.cs
+++ GeoApp/Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace WebApplication1
+namespace GeoApp
 {
     public class Startup
     {
