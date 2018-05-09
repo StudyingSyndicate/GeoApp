08ed6b1a50816abc219a3d9d181f3a964caf16f3
Mike <redrik09@gmail.com>
2018-05-09 12:26:01 +0700

    adding lightbox gallery

--- GeoApp/ClientApp/src/app/app.module.ts
+++ GeoApp/ClientApp/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {
 } from '@angular/material';
 import { OwlModule } from 'ngx-owl-carousel';
 import { GalleryModule } from "@ngx-gallery/core";
+import { LightboxModule } from  '@ngx-gallery/lightbox';
 
 
 import { AppComponent } from './app.component';
@@ -66,6 +67,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';*/
     MatTabsModule,
     OwlModule,
     GalleryModule.forRoot({ gestures:false }),
+    LightboxModule.forRoot(),
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'about', component: AboutComponent },
--- GeoApp/ClientApp/src/app/user/components/home/home.component.ts
+++ GeoApp/ClientApp/src/app/user/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import {Component, AfterViewInit, ViewChild, OnInit} from '@angular/core';
 import { OwlCarousel } from 'ngx-owl-carousel';
-import {GalleryItem, ImageItem} from "@ngx-gallery/core";
+import {Gallery, GalleryItem, ImageItem} from "@ngx-gallery/core";
+import {Lightbox} from "@ngx-gallery/lightbox";
 
 @Component({
   selector: 'app-home',
@@ -43,7 +44,11 @@ export class HomeComponent implements OnInit {
     preview: '../../assets/temp_pics/4.jpg',
   }];
 
+  constructor(public gallery: Gallery, public lightbox: Lightbox) { }
+
   ngOnInit() {
     this.items = this.imageData.map(item => new ImageItem(item.src, item.preview));
+
+    this.gallery.ref('lightbox').load(this.items);
   }
 }
