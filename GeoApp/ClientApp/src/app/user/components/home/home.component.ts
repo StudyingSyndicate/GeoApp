1e2217dff25b30c81b6108f934e340298fcf32c8
god <zarrass@mail.ru>
2018-05-22 10:07:49 +0700

    Owl works on fullscreen

--- GeoApp/ClientApp/src/app/user/components/home/home.component.ts
+++ GeoApp/ClientApp/src/app/user/components/home/home.component.ts
@@ -9,46 +9,19 @@ import {Lightbox} from "@ngx-gallery/lightbox";
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  myOptions = {
-    //autoplay: true,
+   myOptions2 = {
+    autoplay: true,
     loop: true,
     items: 1,
-    dots: true,
-    //dotsContainer: '#carousel-custom-dots'
+    dots: false,
+    dotsContainer: '#carousel-custom-dots',
+    autoWidth: true,
+    center: true,   
   };
-
-  myOptions2 = {
-    //autoplay: true,
-    loop: true,
-    items: 1,
-    dots: true,
-    dotsContainer: '#carousel-custom-dots'
-  };
-
-  items: GalleryItem[];
-
-  imageData = [{
-    src: 'https://i.pinimg.com/originals/0d/e9/bb/0de9bbc33e515c6035078b3b8cdd9f01.jpg',
-    preview: 'https://i.pinimg.com/originals/0d/e9/bb/0de9bbc33e515c6035078b3b8cdd9f01.jpg'
-  }, {
-    src: '../../assets/temp_pics/1.jpg',
-    preview: '../../assets/temp_pics/1.jpg',
-  }, {
-    src: '../../assets/temp_pics/2.jpg',
-    preview: '../../assets/temp_pics/2.jpg',
-  }, {
-    src: '../../assets/temp_pics/3.jpg',
-    preview: '../../assets/temp_pics/3.jpg',
-  }, {
-    src: '../../assets/temp_pics/4.jpg',
-    preview: '../../assets/temp_pics/4.jpg',
-  }];
-
-  constructor(public gallery: Gallery, public lightbox: Lightbox) { }
+  
+  constructor(public) { }
 
   ngOnInit() {
-    this.items = this.imageData.map(item => new ImageItem(item.src, item.preview));
-
-    this.gallery.ref('lightbox').load(this.items);
+   
   }
 }
