d8882fe954b1367592bde3e0cceaa6f2347ac9d3
Mike <redrik09@gmail.com>
2018-07-03 20:12:39 +0700

    fix & rename

--- GeoApp/ClientApp/src/app/user/components/home/home.component.ts
+++ GeoApp/ClientApp/src/app/user/components/home/home.component.ts
@@ -1,27 +1,22 @@
-import {Component, AfterViewInit, ViewChild, OnInit} from '@angular/core';
-import { OwlCarousel } from 'ngx-owl-carousel';
-import {Gallery, GalleryItem, ImageItem} from "@ngx-gallery/core";
-import {Lightbox} from "@ngx-gallery/lightbox";
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-   myOptions2 = {
+export class HomeComponent {
+  public carouselOptions = {
     autoplay: true,
     loop: true,
     items: 1,
     dots: false,
     dotsContainer: '#carousel-custom-dots',
     autoWidth: true,
-    center: true,   
+    center: true,
   };
-  
-  constructor(public) { }
 
-  ngOnInit() {
-   
+  constructor() {
   }
+
 }
