adabf6bdfe6c14bb31f3f95a851ab4f38b9042ef
Zarrass <zarrass@mail.ru>
2018-09-27 10:44:12 +0700

    форматирование

--- GeoApp/ClientApp/src/app/user/components/about/tabs/about-staff/about-staff.component.ts
+++ GeoApp/ClientApp/src/app/user/components/about/tabs/about-staff/about-staff.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-about-staff',
@@ -7,21 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutStaffComponent implements OnInit {
 
-  staffItems =[{
+  staffItems = [{
     name: 'Виктор Совлук',
     position: 'Генеральный директор УК "СибГеоТех"',
-    image:'../../../../../../assets/sovluk.png'
-  },{
-    name:'Александр Совлук',
-    position:'Директор отдела геологоразведочных работ',
-    image:'../../../../../../assets/asovluk.png'
-  },{
-    name:'Хомяков Андрей Сергеевич',
-    position:'Директор отдела инженерных изысканий',
-    image:'../../../../../../assets/male_icon.jpg'
+    image: '../../../../../../assets/sovluk.png'
+  }, {
+    name: 'Александр Совлук',
+    position: 'Директор отдела геологоразведочных работ',
+    image: '../../../../../../assets/asovluk.png'
+  }, {
+    name: 'Хомяков Андрей Сергеевич',
+    position: 'Директор отдела инженерных изысканий',
+    image: '../../../../../../assets/male_icon.jpg'
   }];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
