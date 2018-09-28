66e9bb20f0e9dd7085d6c69ef3114e5c5ffac608
Zarrass <zarrass@mail.ru>
2018-09-28 14:40:15 +0700

    staff_icons

--- GeoApp/ClientApp/src/app/user/components/about/tabs/about-staff/about-staff.component.ts
+++ GeoApp/ClientApp/src/app/user/components/about/tabs/about-staff/about-staff.component.ts
@@ -18,79 +18,79 @@ export class AboutStaffComponent implements OnInit {
   }, {
     name: 'Андрей Сергеевич Хомяков',
     position: 'Директор ООО "Геологические технологии"',
-    image: '../../../../../../assets/male_icon.jpg'
+    image: '../../../../../../assets/male_icon.png'
   }, {
     name: 'Андрей Сергеевич Хомяков',
     position: 'Директор ООО "Геологические технологии"',
-    image: '../../../../../../assets/male_icon.jpg'
+    image: '../../../../../../assets/male_icon.png'
   }, {
     name: 'Качеров Сергей Алексеевич',
     position: 'Директор по экономике и развитию',
-    image: '../../../../../../assets/male_icon.jpg'
+    image: '../../../../../../assets/male_icon.png'
   }, {
     name: 'Звягин Александр Владимирович',
     position: 'Директор по производству',
-    image: '../../../../../../assets/male_icon.jpg'
+    image: '../../../../../../assets/male_icon.png'
   }, {
     name: 'Гриневич Любовь Михайловна',
     position: 'Главный бухгалтер',
-    image: '../../../../../../assets/female_icon.jpg'
+    image: '../../../../../../assets/female_icon.png'
   }, {
     name: 'Киселев Александр Викторович',
     position: 'Главный специалист в отделе инженерных изысканий',
-    image: '../../../../../../assets/male_icon.jpg'
+    image: '../../../../../../assets/male_icon.png'
   }, {
     name: 'Федан Николай Алексеевич',
     position: 'Главный специалист в отделе инженерных изысканий',
-    image: '../../../../../../assets/male_icon.jpg'
+    image: '../../../../../../assets/male_icon.png'
   }, {
     name: 'Сергеев Виктор Витальевич',
     position: 'Инженер-геолог 1 категории',
-    image: '../../../../../../assets/male_icon.jpg'
+    image: '../../../../../../assets/male_icon.png'
   }, {
     name: 'Дебелов Максим Юрьевич',
     position: 'Инженер-эколог',
-    image: '../../../../../../assets/male_icon.jpg'
+    image: '../../../../../../assets/male_icon.png'
   }, {
     name: 'Карпов Геннадий Геньевич',
     position: 'Инженер-геолог 1 категории',
-    image: '../../../../../../assets/male_icon.jpg'
+    image: '../../../../../../assets/male_icon.png'
   }, {
     name: 'Грушецкая Ольга Александровна',
     position: 'Экономист',
-    image: '../../../../../../assets/female_icon.jpg'
+    image: '../../../../../../assets/female_icon.png'
   }, {
     name: 'Тарасов Александр Васильевич',
     position: 'Ведущий геоло',
-    image: '../../../../../../assets/male_icon.jpg'
+    image: '../../../../../../assets/male_icon.png'
   }, {
     name: 'Шведов Геннадий Иванович',
     position: 'Ведущий геолог',
-    image: '../../../../../../assets/male_icon.jpg'
+    image: '../../../../../../assets/male_icon.png'
   }, {
     name: 'Бабинцев Никита Анатольевич',
     position: 'Геолог',
-    image: '../../../../../../assets/male_icon.jpg'
+    image: '../../../../../../assets/male_icon.png'
   }, {
     name: 'Пернова Наталья Васильевна',
     position: 'Ведущий эколог',
-    image: '../../../../../../assets/female_icon.jpg'
+    image: '../../../../../../assets/female_icon.png'
   }, {
     name: 'Подковальникова Полина Дмитриевна',
     position: 'Геолог',
-    image: '../../../../../../assets/female_icon.jpg'
+    image: '../../../../../../assets/female_icon.png'
   }, {
     name: 'Нифатова Светлана Руслановна',
     position: 'Геолог',
-    image: '../../../../../../assets/female_icon.jpg'
+    image: '../../../../../../assets/female_icon.png'
   }, {
     name: 'Смоленскас Владас Вито',
     position: 'Геолог',
-    image: '../../../../../../assets/male_icon.jpg'
+    image: '../../../../../../assets/male_icon.png'
   }, {
     name: 'Подковальников Александр Александрович',
     position: 'Маркшейдер 1 категории',
-    image: '../../../../../../assets/male_icon.jpg'
+    image: '../../../../../../assets/male_icon.png'
   }];
 
   constructor() {
